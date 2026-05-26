2ab85e8938728da47b5de758b95590d058a4fbf7
XniceCraft <xnicecraftacc@gmail.com>
2026-05-26 09:26:45 +0700

    fix: fix build error caused by qs access on SSR

--- src/app/my-documents/page.tsx
+++ src/app/my-documents/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { Content } from "./_components/content";
 import { MenuBar } from "./_components/layout/menu-bar";
 import { MainLayout } from "@/components/layout/main-layout";
@@ -11,13 +12,17 @@ export const metadata: Metadata = {
 export default function MyDocumentPage() {
   return (
     <MainLayout>
-      <MenuBar />
+      <Suspense>
+        <MenuBar />
+      </Suspense>
       <div className="max-w-5xl mx-auto w-full px-8 pt-8">
         <h1 className="text-3xl font-semibold mb-2">My Documents</h1>
         <p className="text-sm font-light mb-5 text-muted-foreground">
           Manage and continue editing your scanned files.
         </p>
-        <Content />
+        <Suspense>
+          <Content />
+        </Suspense>
       </div>
     </MainLayout>
   );
